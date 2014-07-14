058d6f30b56e943d0cb195e09da6fdb38559ec29
bogdan.margarit <margarit.bogdan@yahoo.com>
2014-07-14 12:06:54 +0300

    primul update

---
+++ tutorial_quickstart/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'database.sql',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
+    }
+}
